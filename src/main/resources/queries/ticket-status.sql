48ba789e8403fbc1a9e96882fcbbc04d49247669
Ludvinae <benoit.champlon79@gmail.com>
2026-03-22 12:05:24 +0100

    chore: filtered some info on ticket-status query

--- src/main/resources/queries/ticket-status.sql
+++ src/main/resources/queries/ticket-status.sql
@@ -1,3 +1,5 @@
-SELECT * FROM ticket AS t
+SELECT t.description, c.email, s.designation, h.start_date, h.end_date
+FROM ticket AS t
 JOIN history h on t.id_ticket = h.id_ticket
-JOIN status s ON h.id_status = s.id_status;
+JOIN status s ON h.id_status = s.id_status
+JOIN client c ON t.id_client = c.id_client;

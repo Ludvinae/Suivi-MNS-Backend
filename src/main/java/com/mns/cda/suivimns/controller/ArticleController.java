555f13a1bb1211cc3d26569880dbc5fd7db1c619
Ludvinae <benoit.champlon79@gmail.com>
2026-03-23 09:20:29 +0100

    feat(controller): added Article and Assignment entities controllers

--- src/main/java/com/mns/cda/suivimns/controller/ArticleController.java
+++ src/main/java/com/mns/cda/suivimns/controller/ArticleController.java
@@ -1,8 +1,17 @@
 package com.mns.cda.suivimns.controller;
 
 import com.mns.cda.suivimns.dao.ArticleDao;
+import com.mns.cda.suivimns.model.Article;
+import com.mns.cda.suivimns.model.groups.OnCreate;
+import com.mns.cda.suivimns.model.groups.OnUpdate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ArticleController {
@@ -12,5 +21,60 @@ public class ArticleController {
     @Autowired
     public ArticleController(ArticleDao articleDao) {
         this.articleDao = articleDao;
+    }
+
+    @GetMapping("/article/list")
+    public List<Article> getAll() {
+        return articleDao.findAll();
+    }
+
+    @GetMapping("/article/{id}")
+    public ResponseEntity<Article> getById(@PathVariable int id) {
+        Optional<Article> article = articleDao.findById(id);
+
+        if (article.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(article.get(), HttpStatus.OK);
+    }
+
+    @PostMapping("/article")
+    public ResponseEntity<Article> create(@RequestBody @Validated(OnCreate.class) Article article) {
+        article.setIdArticle(null);
+        article.setModificationDate(null);
+
+        articleDao.save(article);
+
+        return new ResponseEntity<>(article, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/article/{id}")
+    public ResponseEntity<Article> delete(@PathVariable int id) {
+        Optional<Article> article = articleDao.findById(id);
+
+        if (article.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        articleDao.delete(article.get());
+        return new ResponseEntity<>(article.get(), HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping("/article/{id}")
+    public ResponseEntity<Article> update(@PathVariable int id, @RequestBody @Validated(OnUpdate.class) Article articleToUpdate) {
+        Optional<Article> article = articleDao.findById(id);
+
+        if (article.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        if (articleToUpdate.getContent() == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        articleToUpdate.setCreationDate(null);
+        articleDao.save(articleToUpdate);
+        return new ResponseEntity<>(article.get(), HttpStatus.OK);
     }
 }
--- src/main/java/com/mns/cda/suivimns/controller/AssignmentController.java
+++ src/main/java/com/mns/cda/suivimns/controller/AssignmentController.java
@@ -1,8 +1,15 @@
 package com.mns.cda.suivimns.controller;
 
 import com.mns.cda.suivimns.dao.AssignmentDao;
+import com.mns.cda.suivimns.model.Assignment;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class AssignmentController {
@@ -13,4 +20,54 @@ public class AssignmentController {
     public AssignmentController(AssignmentDao assignmentDao) {
         this.assignmentDao = assignmentDao;
     }
+
+    @GetMapping("/assignment/list")
+    public List<Assignment> getAll() {
+        return assignmentDao.findAll();
+    }
+
+    @GetMapping("/assignment/{id}")
+    public ResponseEntity<Assignment> getById(@PathVariable int id) {
+        Optional<Assignment> assignment = assignmentDao.findById(id);
+        if (assignment.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(assignment.get(), HttpStatus.OK);
+    }
+
+    @PostMapping("/assignment")
+    public ResponseEntity<Assignment> create(@RequestBody @Validated() Assignment assignment) {
+        assignment.setIdAssignment(null);
+        assignmentDao.save(assignment);
+
+        return new ResponseEntity<>(assignment, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/assignment/{id}")
+    public ResponseEntity<Assignment> delete(@PathVariable int id) {
+        Optional<Assignment> assignment = assignmentDao.findById(id);
+
+        if (assignment.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        assignmentDao.delete(assignment.get());
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping("/assignment/{id}")
+    public ResponseEntity<Assignment> update(@PathVariable int id, @RequestBody @Validated() Assignment assignmentToUpdate) {
+        Optional<Assignment> assignment = assignmentDao.findById(id);
+
+        if (assignment.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        assignmentToUpdate.setIdAssignment(id);
+        assignmentDao.save(assignmentToUpdate);
+
+        return new ResponseEntity<>(assignmentToUpdate, HttpStatus.OK);
+    }
+
 }

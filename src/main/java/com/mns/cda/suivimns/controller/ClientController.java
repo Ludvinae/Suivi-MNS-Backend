8af25b307f72d09fe12cd2161230b7b815b01a38
Ludvinae <benoit.champlon79@gmail.com>
2026-03-23 10:31:22 +0100

    feat(controller): added controller for Client entity. Added test for Assignment model

--- src/main/java/com/mns/cda/suivimns/controller/ClientController.java
+++ src/main/java/com/mns/cda/suivimns/controller/ClientController.java
@@ -1,8 +1,17 @@
 package com.mns.cda.suivimns.controller;
 
 import com.mns.cda.suivimns.dao.ClientDao;
+import com.mns.cda.suivimns.model.Client;
+import com.mns.cda.suivimns.model.groups.OnCreate;
+import com.mns.cda.suivimns.model.groups.OnUpdate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ClientController {
@@ -12,5 +21,54 @@ public class ClientController {
     @Autowired
     public ClientController(ClientDao clientDao) {
         this.clientDao = clientDao;
+    }
+
+    @GetMapping("/client/list")
+    public List<Client> getAll() {
+        return clientDao.findAll();
+    }
+
+    @GetMapping("/client/{id}")
+    public ResponseEntity<Client> getById(@PathVariable int id) {
+
+        Optional<Client> client = clientDao.findById(id);
+        if (client.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(client.get(), HttpStatus.OK);
+    }
+
+    @PostMapping("/client")
+    public ResponseEntity<Client> create(@RequestBody @Validated(OnCreate.class) Client client) {
+        client.setIdClient(null);
+        clientDao.save(client);
+
+        return new ResponseEntity<>(client, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/client/{id}")
+    public ResponseEntity<Client> delete(@PathVariable int id) {
+        Optional<Client> client = clientDao.findById(id);
+        if (client.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        clientDao.delete(client.get());
+        return new ResponseEntity<>(client.get(), HttpStatus.OK);
+    }
+
+    @PutMapping("/client/{id}")
+    public ResponseEntity<Client> update(@PathVariable int id, @RequestBody @Validated(OnUpdate.class) Client clientToUpdate) {
+        Optional<Client> client = clientDao.findById(id);
+
+        if (client.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        clientToUpdate.setIdClient(client.get().getIdClient());
+        clientToUpdate.setPassword(client.get().getPassword());
+        clientDao.save(clientToUpdate);
+        return new ResponseEntity<>(client.get(), HttpStatus.OK);
     }
 }
---
+++ src/test/java/com/mns/cda/suivimns/unit/model/AssignmentUnitTest.java
@@ -0,0 +1,39 @@
+package com.mns.cda.suivimns.unit.model;
+
+import com.mns.cda.suivimns.model.Assignment;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class AssignmentUnitTest {
+
+    protected static Validator validator;
+
+    @BeforeAll
+    public static void init() {
+        validator = Validation.buildDefaultValidatorFactory().getValidator();
+    }
+
+    @Test
+    public void assignmentWithEmptyData_shouldBeValid() {
+        Assignment assignment = new Assignment();
+
+        boolean hasViolation = !validator.validate(assignment).isEmpty();
+
+        Assertions.assertFalse(hasViolation, "Assignment should be valid because no constraints are defined");
+    }
+
+    @Test
+    public void assignmentWithAllFieldsFilled_shouldBeValid() {
+        Assignment assignment = new Assignment();
+
+        assignment.setAssigmentDate(null); // auto généré normalement
+        assignment.setEndDate(null);
+
+        boolean hasViolation = !validator.validate(assignment).isEmpty();
+
+        Assertions.assertFalse(hasViolation, "Assignment should always be valid");
+    }
+}

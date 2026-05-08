d7a4f5e334834423bc9a7e8192b3e46661bd1cda
Ludvinae <benoit.champlon79@gmail.com>
2026-05-08 18:12:14 +0200

    test(status): correctly inject MockBean for StatusTransition service in StatusControllerUnitTest. Added tests for new getPossibleTransition API route

--- src/main/java/com/mns/cda/suivimns/service/business/StatusTransition.java
+++ src/main/java/com/mns/cda/suivimns/service/business/StatusTransition.java
@@ -1,9 +1,6 @@
 package com.mns.cda.suivimns.service.business;
 
-import com.mns.cda.suivimns.dao.StatusDao;
 import com.mns.cda.suivimns.enumerate.StatusEnum;
-import com.mns.cda.suivimns.model.Status;
-import com.mns.cda.suivimns.service.StatusService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
--- src/test/java/com/mns/cda/suivimns/unit/controller/StatusControllerUnitTest.java
+++ src/test/java/com/mns/cda/suivimns/unit/controller/StatusControllerUnitTest.java
@@ -5,6 +5,7 @@ import com.mns.cda.suivimns.controller.StatusController;
 import com.mns.cda.suivimns.dto.StatusDto;
 import com.mns.cda.suivimns.enumerate.StatusEnum;
 import com.mns.cda.suivimns.service.StatusService;
+import com.mns.cda.suivimns.service.business.StatusTransition;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +15,9 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
+import java.util.Set;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -29,6 +32,9 @@ class StatusControllerUnitTest {
 
     @MockBean
     private StatusService statusService;
+
+    @MockBean
+    private StatusTransition transition;
 
     @MockBean
     private org.springframework.data.jpa.mapping.JpaMetamodelMappingContext jpaMetamodelMappingContext;
@@ -73,6 +79,40 @@ class StatusControllerUnitTest {
                 .andExpect(jsonPath("$[0].designation").value("Test designation"))
                 .andExpect(jsonPath("$[0].code").value(StatusEnum.OPEN.name()));
     }
+
+    // =========================
+    // GET POSSIBLE TRANSITIONS - CORRECT SET
+    // =========================
+    @Test
+    void shouldReturnAllowedTransitions() throws Exception {
+
+        Set<StatusEnum> transitions = Set.of(
+                StatusEnum.ASSIGNED,
+                StatusEnum.REJECTED
+        );
+
+        when(transition.getAllowedTransitions(StatusEnum.OPEN))
+                .thenReturn(transitions);
+
+        mockMvc.perform(get("/status/allowed-transitions/OPEN"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0]").exists())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$").isArray());
+    }
+
+
+    // =========================
+    // GET POSSIBLE TRANSITIONS - INVALID STATUS
+    // =========================
+    @Test
+    void shouldReturnBadRequestForInvalidStatus() throws Exception {
+
+        mockMvc.perform(get("/status/allowed-transitions/INVALID"))
+                .andExpect(status().isBadRequest());
+    }
+
 
     // =========================
     // GET BY ID - OK

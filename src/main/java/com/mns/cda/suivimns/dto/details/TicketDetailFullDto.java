df31a04da0e86e52feac25073243869c99fbc447
Ludvinae <benoit.champlon79@gmail.com>
2026-05-28 08:51:35 +0200

    feat(ticket): added possible status accessible to transition to from current status in ticket detail dto

--- src/main/java/com/mns/cda/suivimns/dto/details/TicketDetailFullDto.java
+++ src/main/java/com/mns/cda/suivimns/dto/details/TicketDetailFullDto.java
@@ -1,11 +1,15 @@
 package com.mns.cda.suivimns.dto.details;
 
+import com.mns.cda.suivimns.enumerate.StatusEnum;
+
 import java.util.List;
+import java.util.Set;
 
 public record TicketDetailFullDto(
         TicketDetailDto details,
         List<TicketDetailComment> comments,
         TicketDetailKnowledge knowledge,
-        List<TicketDetailArticle> articles
+        List<TicketDetailArticle> articles,
+        Set<StatusEnum> possibleStatusTransitions
 ) {
 }
--- src/main/java/com/mns/cda/suivimns/service/business/TicketDetailService.java
+++ src/main/java/com/mns/cda/suivimns/service/business/TicketDetailService.java
@@ -2,16 +2,20 @@ package com.mns.cda.suivimns.service.business;
 
 import com.mns.cda.suivimns.dao.TicketDao;
 import com.mns.cda.suivimns.dto.details.*;
+import com.mns.cda.suivimns.enumerate.StatusEnum;
+import com.mns.cda.suivimns.service.workflow.StatusTransition;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
 public class TicketDetailService {
 
     private final TicketDao ticketDao;
+    private final StatusTransition transition;
 
     public TicketDetailFullDto getTicketDetails(Integer idTicket) {
         TicketDetailDto details = ticketDao.ticketDetail(idTicket);
@@ -24,7 +28,8 @@ public class TicketDetailService {
         }
 
         List<TicketDetailComment> comments = ticketDao.ticketDetailComments(idTicket);
+        Set<StatusEnum> statusList = transition.getAllowedTransitions(details.currentStatus());
 
-        return new  TicketDetailFullDto(details, comments, knowledge, articles);
+        return new  TicketDetailFullDto(details, comments, knowledge, articles, statusList);
     }
 }
